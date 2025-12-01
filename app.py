585e38276c517e6a1665c4fa15e40daeb840b482
Bhagya-Shah <91955020+Bhagya-Shah@users.noreply.github.com>
2025-12-01 13:31:47 +0000

    commit

--- app.py
+++ app.py
@@ -10,7 +10,19 @@ st.set_page_config(page_title="Room Allotment", layout="wide")
 # Load credentials + gspread ONLY ONCE (session_state)
 # -------------------------------
 if "client" not in st.session_state:
-    creds_dict = json.loads(st.secrets["GOOGLE_CREDS"])
+    creds_dict = {
+    "type": st.secrets["type"],
+    "project_id": st.secrets["project_id"],
+    "private_key_id": st.secrets["private_key_id"],
+    "private_key": st.secrets["private_key"],
+    "client_email": st.secrets["client_email"],
+    "client_id": st.secrets["client_id"],
+    "auth_uri": st.secrets["auth_uri"],
+    "token_uri": st.secrets["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+    "universe_domain": st.secrets["universe_domain"],
+    }
     creds = Credentials.from_service_account_info(
         creds_dict,
         scopes=[
